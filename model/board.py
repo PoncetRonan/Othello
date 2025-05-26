0bd05c5cbd7b7aadc70f33d7cc963478b3fc2a4d
Diletta Ciardo <dilettaciardo@yahoo.it>
2025-05-26 14:41:15 +0200

    board_skeleton

---
+++ model/board.py
@@ -0,0 +1,25 @@
+class Board:
+    def __init__(self, grid, score):
+        self._grid = grid
+        self._score = score
+
+    def get_grid(self):
+        pass
+        
+    def get_score(self):
+        pass
+    
+    def verify_possible_move(self):
+        pass
+
+    def play_pawn(self):
+        pass
+
+    def update_board(self):
+        pass
+
+    def calculate_move(self):
+        pass
+
+    def calculate_score(self):
+        pass
