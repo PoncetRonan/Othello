c180f5fa29da312afe9c4cb1d0361647c90bb3cb
Jip Wulffelé <jipwulffele@gmail.com>
2025-05-26 14:39:20 +0200

    Create skelet othello.py

---
+++ engine/othello.py
@@ -0,0 +1,29 @@
+class Othello():
+    def __init__(self, continue_game=True, current_player="black"):
+        self._continue_game = continue_game
+
+        if current_player in ["white", "black"]:
+            self._current_player = current_player
+        else:
+            raise ValueError("Value is not valid. Valid values are: 'white', 'black'")
+        
+        self._num_blocked_players = 0
+        self._possible_moves = None
+
+    def start_game(self):
+        pass
+
+    def determine_next_player(self):
+        pass
+
+    def play_game(self):
+        pass
+
+    def determine_next_move(self):
+        pass
+
+    def make_move(self):
+        pass
+
+    def display_current_state(self):
+        pass
