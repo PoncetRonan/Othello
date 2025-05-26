6c2897753847574b0edc8962c3f43f173980a9c8
Ronan <ronan.poncet@le-campus-numerique.fr>
2025-05-26 14:50:05 +0200

    ajout squelette display.py

---
+++ view/display.py
@@ -0,0 +1,36 @@
+class display():
+
+
+
+
+    def __init__(self):
+            self._black=None
+            self._white=None
+            
+    @property
+    def white(self):
+        return self._white
+    
+    @property
+    def black(self):
+        return self._white
+    
+
+    def welcome(self,nom_joueur1,nom_joueur2):
+        pass
+
+    def imput_player():
+        pass
+
+    def imput_play_move(self,nom_joueur):
+        #return imput
+        pass
+
+    def print_board(self,board):
+        pass
+
+    def print_score(self,score):
+        pass
+
+    def end_message(self):
+        pass
