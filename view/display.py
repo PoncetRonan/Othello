81a13cb962ddf1902815d73ddaa4c221a05c256e
Ronan <ronan.poncet@le-campus-numerique.fr>
2025-05-26 16:34:15 +0200

    modif gitignore + ajout welcome and input in display.py

--- view/display.py
+++ view/display.py
@@ -13,21 +13,36 @@ class Display():
     
     @property
     def black(self):
-        return self._white
+        return self._black
     
 
     def welcome(self):
-        pass
+        print('Welcome')
+        self.input_player()
 
-    def imput_player():
-        pass
 
-    def imput_play_move(self):
-        #return imput
-        pass
+    def input_player(self):
+        self._black=input("Enter the first player's name :  \n")
+        self._white=input("Enter the second player's name : \n")
+
+    def imput_play_move(self,list_valid_move):
+        is_valid=False
+        next_move=input('Enter your next move')
+        if next_move in list_valid_move:
+            is_valid=True
+        while is_valid == False:
+            next_move=input('Incorrect move , enter a valid move')
+            if next_move in list_valid_move:
+                is_valid=True
+        return next_move
+
+            
+
+        
 
     def print_board(self,board):
         pass
+        
 
     def print_score(self,score):
         pass
