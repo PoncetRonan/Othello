9390dab5c99353bd31c691db07a6de8789dabbcb
Diletta Ciardo <dilettaciardo@yahoo.it>
2025-05-27 10:42:35 +0200

    add _is_valid_move in board.py

--- model/board.py
+++ model/board.py
@@ -26,32 +26,71 @@ class Board:
         # case and neighbor on same row; neighbore is on the right of case
         if ((neighbor[0] == case[0]) and (neighbor[0] < case[0])):
             for j in range(case[1] + 1, 8):
-                if self._grid[case[0], j].state == 'occupied':
+                while self._grid[case[0], j].state == 'occupied':
                     if self._grid[case[0], j].pawn.color == current_player:
                         result = True
                         break
+
         # case and neighbor on same row; neighbore is on the left of case
         if ((neighbor[0] == case[0]) and (neighbor[0] > case[0])):
             for j in range(0, case[1]):
-                if self._grid[case[0], j].state == 'occupied':
+                while self._grid[case[0], j].state == 'occupied':
                     if self._grid[case[0], j].pawn.color == current_player:
                         result = True
                         break
+
         # case and neighbor on same column; neighbor is above case
         if ((neighbor[1] == case[1]) and (neighbor[1] > case[1])):
             for i in range(case[0] + 1, 8):
-                if self._grid[i, case[1]].state == 'occupied':
+                while self._grid[i, case[1]].state == 'occupied':
                     if self._grid[i, case[1]].pawn.color == current_player:
                         result = True
                         break
+
         # case and neighbor on same column; neighbor is below case
         if ((neighbor[1] == case[1]) and (neighbor[1] < case[1])):
             for i in range(0, case[0]):
-                if self._grid[i, case[1]].state == 'occupied':
+                while self._grid[i, case[1]].state == 'occupied':
                     if self._grid[i, case[1]].pawn.color == current_player:
                         result = True
                         break
-        # case and neighbor on diagonal; neighbor is above and to the left of case
+
+        # case and neighbor on diagonal; neighbor is below and to the right of case
+        if ((neighbor[0] > case[0]) and (neighbor[1] > case[1])):
+            i = case[0] - 1
+            j = case[1] - 1
+            while ((i >= 0) and (j >= 0)):
+                while self._grid[i, j].state == 'occupied':
+                    if self._grid[i, j].pawn.color == current_player:
+                        result = True
+                        break
+                i -= 1
+                j -= 1
+
+        # case and neighbor on diagonal; neighbor is below and to the left of case
+        if ((neighbor[0] > case[0]) and (neighbor[1] < case[1])):
+            i = case[0] - 1
+            j = case[1] + 1
+            while ((i >= 0) and (j < 8)):
+                while self._grid[i, j].state == 'occupied':
+                    if self._grid[i, j].pawn.color == current_player:
+                        result = True
+                        break
+                i -= 1
+                j += 1
+        
+        # case and neighbor on diagonal; neighbor is above and to the right of case
+        if ((neighbor[0] < case[0]) and (neighbor[1] > case[1])):
+            i = case[0] + 1
+            j = case[1] - 1
+            while ((i < 8) and (j >= 0)):
+                while self._grid[i, j].state == 'occupied':
+                    if self._grid[i, j].pawn.color == current_player:
+                        result = True
+                        break
+                i += 1
+                j -= 1
+
 
 
 
