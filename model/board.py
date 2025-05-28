ab06038836d7dbef13f0ffd52ac02d74d96e5e43
Jip Wulffelé <jipwulffele@gmail.com>
2025-05-28 11:33:55 +0200

    Board.py works now

--- model/board.py
+++ model/board.py
@@ -15,8 +15,6 @@ class Board:
         self._grid[3, 4].add_pawn(Pawn('black'))
         self._grid[4, 3].add_pawn(Pawn('black'))
         self._grid[4, 4].add_pawn(Pawn('white'))
-
-
         self._score_white = 0
         self._score_black = 0
 
@@ -64,10 +62,21 @@ class Board:
         return direction
     
     def increment_direction(self, move, direction):
+        """
+        Increments the coordinates of a move in a specified direction
+        AND check that the incremented moves are within the board.
+        The function returns a boolean indicating if the move is valid and the new coordinates.
+        If the move is out of bounds, it returns the original coordinates.
+        Args:
+            move (tuple): Coordinates of the move (row, col).
+            direction (str): Direction of the move ('up', 'down', 'left', 'right', 'up-left', 'up-right', 'down-left', 'down-right').
+        Returns:
+            tuple: A tuple containing a boolean indicating if the move is valid and the new coordinates (row, col).
+        """
         i = move[0]
         j = move[1]
+        valid = True
         if i >= 0 and i < 8 and j >= 0 and j < 8:
-            valid = True
             if direction == 'up':
                 i -= 1
             elif direction == 'down':
@@ -88,14 +97,22 @@ class Board:
             elif direction == 'down-right':
                 i += 1
                 j += 1
-        else:
+        if i < 0 or i >= 8 or j < 0 or j >= 8:
             valid = False
-
-            # Check that i,j is withi ngrid
-            # else return False
+            i = move[0]
+            j = move[1]
         return valid, (i, j)
     
     def is_valid_move(self, move, direction, color):
+        """
+        Checks if a move is valid according to the Othello rules.
+        Args:
+            move (tuple): Coordinates of the move (row, col).
+            direction (str): Direction of the move ('up', 'down', 'left', 'right', 'up-left', 'up-right', 'down-left', 'down-right').
+            color (str): Color of the pawn ('white' or 'black').
+        Returns:
+            bool: True if the move is valid, False otherwise.
+        """
         result = False
         valid, (i, j) = self.increment_direction(move , direction)
         while ((valid) & (self._grid[i, j].state == 'occupied')):
@@ -106,6 +123,15 @@ class Board:
         return result
 
     def verify_possible_move(self, current_player):
+        """
+        Verifies all possible moves for the current player.
+        Iterates through the grid and checks each occupied cell for potential moves.
+        Then checks if the move is valid according to the Othello rules.
+        Args:
+            current_player (str): Color of the current player ('white' or 'black').
+        Returns:
+            list: A list of tuples representing the coordinates of valid moves for the current player.
+        """
         possible_moves = []
         for i in range(8):
             for j in range(8):
@@ -117,9 +143,29 @@ class Board:
         return list(set(possible_moves))  # Remove duplicates
                 
     def play_pawn(self, row, col, current_player):
+        """
+        Places a pawn on the board at the specified row and column for the current player.
+        Args:
+            row (int): Row index where the pawn will be placed.
+            col (int): Column index where the pawn will be placed.
+            current_player (str): Color of the current player ('white' or 'black').
+        Returns:
+            None
+        """
         self._grid[row, col].add_pawn(Pawn(current_player))
 
     def update_board(self, row, col, current_player):
+        """
+        Updates the board by swapping the colors of pawns that are turned over
+        after a pawn is placed at the specified row and column for the current player.
+        Then calculates the score for both players.
+        Args:
+            row (int): Row index where the pawn will be placed.
+            col (int): Column index where the pawn will be placed.
+            current_player (str): Color of the current player ('white' or 'black').
+        Returns:
+            None
+        """
         pawn_to_turn = self.calculate_move(row, col, current_player)
         for (i, j) in pawn_to_turn:
             self._grid[i, j].pawn.swap_color()
@@ -135,26 +181,19 @@ class Board:
                     self._grid[i+row,j+col].pawn.color
                 except:
                     continue    
-
                 if (self._grid[i+row,j+col].pawn.color != color):
                     flag_pawn=False
                     n=1
-                    while (n*i+row < 8) and (n*j+col < 8) and (n*i+row >-1) and (n*j+col > -1) and flag_pawn==False:
-                        
+                    while (n*i+row < 8) and (n*j+col < 8) and (n*i+row >-1) and (n*j+col > -1) and flag_pawn==False:                       
                         try:
                             self._grid[n*i+row,n*j+col].pawn.color
                         except:
                             flag_pawn=True
                             continue
-                            
-
-
-                        if self._grid[n*i+row,n*j+col].pawn.color == color:
-                            
+                        if self._grid[n*i+row,n*j+col].pawn.color == color:                            
                             pawn_to_turn.extend((row + m*i, col + m*j) for m in range(1, n))
                             flag_pawn=True
                         else:
-
                             n=n+1
                 else:
                     pass 
@@ -172,7 +211,6 @@ class Board:
         Returns:
             None
         """
-        
         score_white = 0
         score_black = 0
 
